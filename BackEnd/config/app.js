6f53a77ff79941d378cf1f776b9941ed778ad778
MGShio <maxenceguenaou@yahoo.ca>
2024-07-05 13:52:30 +0200

    Images are loading
    I tried many thing, but it was a path error again.
    
    The image are loading correctly

--- BackEnd/config/app.js
+++ BackEnd/config/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require('path');
+require('dotenv').config(); // Load .env file
 
 app.use(cors());
 app.use(express.json());
 
 // Serve static files from the "public" folder
-app.use("/images", express.static(__dirname + "/../public/images"));
+const imagesFolder = process.env.IMAGES_FOLDER;
+app.use("/" + process.env.IMAGES_PUBLIC_URL, express.static(path.join(__dirname, imagesFolder)));
 
 module.exports = { app };
